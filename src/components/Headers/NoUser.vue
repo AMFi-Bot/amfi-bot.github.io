f90b56beb83ce08a6360d320f1a7f714084d98f4
Vetrov Vadim <vetrovvd@yandex.ru>
2022-08-01 14:43:24 +0300

    added login logic to component

--- src/components/Headers/NoUser.vue
+++ src/components/Headers/NoUser.vue
@@ -6,7 +6,7 @@ const userStore = useUserStore();
 
 onMounted(() => {
   userStore.load_telegram_widget_script("telegram_auth_mount_point");
-}); //
+});
 </script>
 
 <template>
@@ -14,7 +14,7 @@ onMounted(() => {
     <div class="login_type telegram telegram_auth_mount_point">
       <div>Loading telegram auth form...</div>
     </div>
-    <div class="login_type discord">
+    <div class="login_type discord" @click="userStore.login_discord()">
       <img src="/img/dslogo_white.svg" alt="" />
       <label class="login_title">Log in with Discord</label>
     </div>
