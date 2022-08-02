15da75821821f434ccce0d6d183add16f9bc98d1
Waujito <VetrovVD@yandex.ru>
2022-08-02 10:33:57 +0000

    Deploying to gh-pages from @ AMFi-Bot/amfi-bot.github.io@c1d062919ebfa67c3ed3d1987133e55ed50d8a7c 🚀

---
+++ assets/DiscordAuth.4daff4f1.js
@@ -0,0 +1,1 @@
+import{d as s,u as a,w as c,c as n,o as r,L as _}from"./index.20de96dc.js";const l=s({__name:"DiscordAuth",async setup(i){let e,o;const t=a();return[e,o]=c(()=>t.discord_callback(window.location.search)),await e,o(),(p,d)=>(r(),n(_))}});export{l as default};
