7805fdac29ce070d074a85da5054f072514c12b4
Vetrov Vadim <vetrovvd@yandex.ru>
2022-08-06 12:21:09 +0300

    added guild modules types

--- src/types/discord/guild.d.ts
+++ src/types/discord/guild.d.ts
@@ -1,15 +1,50 @@
-export type DiscordGuild = {
+export type DiscordUserGuild = {
   name: string;
   id: string | number;
   icon: string;
   bot_exists: boolean;
 };
 
-export type DiscordBotGuild = {
+export type DiscordGuildChannel = {
+  id: string;
+  guild_id: string;
+  type: number;
+  name: string;
+};
+
+export type DiscordGuildRole = {
+  id: string;
+  guild_id: string;
+  name: string;
+  permissions: number;
+  position: number;
+  color: number;
+  hoist: boolean;
+  managed: boolean;
+  mentionable: boolean;
+  icon?: string;
+  unicode_emoji?: boolean;
+  flags: number;
+};
+
+export type DiscordBaseGuild = {
   id: string;
   name: string;
   icon?: string;
-  module_general: any;
-  channels: any;
-  roles: any;
+  channels: DiscordGuildChannel[];
+  roles: DiscordGuildRole[];
+};
+
+export type DiscordGuildGeneralModule = {
+  id: string;
+  logChannel?: string;
+  logEnabled?: boolean;
+  logTypes?: string[];
+  [x: string]: any;
+  created_at?: string;
+  updated_at?: string;
+};
+
+export type DiscordFullGuild = DiscordBaseGuild & {
+  module_general?: DiscordGuildGeneralModule;
 };
