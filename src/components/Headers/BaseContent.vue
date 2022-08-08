ee6865adcafe2cab00ce5f51d6c304ac719be272
Vetrov Vadim <vetrovvd@yandex.ru>
2022-08-08 18:57:21 +0300

    added adaptive design support for menu

--- src/components/Headers/BaseContent.vue
+++ src/components/Headers/BaseContent.vue
@@ -1,17 +1,28 @@
+<script setup lang="ts">
+import DropdownComponent from "../DropdownComponent.vue";
+</script>
+
 <template>
   <div class="header">
     <div class="menu_panel">
-      <RouterLink class="bot_name" to="/">AMFi-Bot</RouterLink>
-      <div class="menu_ops">
-        <a href="/tutorials">
-          <span>{{ $t("tutorials") }}</span>
-        </a>
+      <DropdownComponent
+        position="bottom-left"
+        :elem_class="'menu_dropdown'"
+        :content_class="'menu_ops'"
+      >
+        <template #dropdownTitle>...</template>
+        <template #default>
+          <a href="/tutorials">
+            <span>{{ $t("tutorials") }}</span>
+          </a>
 
-        <a href="/premium" class="premium">
-          <span>{{ $t("premium") }}</span>
-          <img src="/img/premium-yellow.svg" class="premium_icon" alt="" />
-        </a>
-      </div>
+          <a href="/premium" class="premium">
+            <span>{{ $t("premium") }}</span>
+            <img src="/img/premium-yellow.svg" class="premium_icon" alt="" />
+          </a>
+        </template>
+      </DropdownComponent>
+      <RouterLink class="bot_name" to="/">AMFi-Bot</RouterLink>
     </div>
     <div>
       <slot />
