11c9a4dc27bbcc411e0e9d641ad744ce5644527c
Vetrov Vadim <vetrovvd@yandex.ru>
2022-08-08 15:48:44 +0300

    rewrote base user element from v-tippy to dropdown component

--- src/components/Headers/BaseUser.vue
+++ src/components/Headers/BaseUser.vue
@@ -1,14 +1,62 @@
 <script setup lang="ts">
 import { useUserStore } from "@/stores/user.js";
-import { setI18nLanguage } from "@/i18n";
-import { useI18n } from "vue-i18n";
+import DropdownComponent from "../DropdownComponent.vue";
+import LanguagesContent from "../LanguagesContent.vue";
 
 const userStore = useUserStore();
-
-const i18n = useI18n();
 </script>
 
 <template>
+  <DropdownComponent
+    position="bottom-right"
+    elem_class="user"
+    content_class="cart"
+    :no-hide-on-click-content="true"
+  >
+    <template #dropdownTitle>
+      <img class="avatar" :src="userStore.avatar" v-if="userStore.avatar" />
+      <div class="name">
+        {{ userStore.name }}
+        <div class="type" v-if="userStore.privilege !== 0">
+          <img
+            height="20px"
+            width="20px"
+            v-bind:src="
+              userStore.privilege === -1
+                ? '/img/administrator.svg'
+                : '/img/premium-yellow.svg'
+            "
+            v-tippy="{
+              content:
+                userStore.privilege === -1
+                  ? 'Администратор'
+                  : 'Привелегированый пользователь',
+            }"
+          />
+        </div>
+      </div>
+    </template>
+    <template #default>
+      <RouterLink to="/dashboard" class="cart_link">
+        {{ $t("dashboard") }}
+      </RouterLink>
+      <RouterLink to="/premium" class="cart_link">{{
+        $t("premium")
+      }}</RouterLink>
+      <RouterLink to="/user/account/settings" class="cart_link">
+        {{ $t("user_settings") }}
+      </RouterLink>
+
+      <DropdownComponent position="left" content_class="cart_languages">
+        <template #dropdownTitle> {{ $t("language") }}/Language </template>
+        <template #default><LanguagesContent /></template>
+      </DropdownComponent>
+
+      <div @click="userStore.logout()" class="cart_link logout_link">
+        {{ $t("log_out") }}
+      </div>
+    </template>
+  </DropdownComponent>
   <tippy
     class="user"
     v-tippy="{
@@ -17,90 +65,8 @@ const i18n = useI18n();
       placement: 'bottom',
     }"
   >
-    <img class="avatar" :src="userStore.avatar" v-if="userStore.avatar" />
-    <div class="name">
-      {{ userStore.name }}
-      <div class="type" v-if="userStore.privilege !== 0">
-        <img
-          height="20px"
-          width="20px"
-          v-bind:src="
-            userStore.privilege === -1
-              ? '/img/administrator.svg'
-              : '/img/premium-yellow.svg'
-          "
-          v-tippy="{
-            content:
-              userStore.privilege === -1
-                ? 'Администратор'
-                : 'Привелегированый пользователь',
-          }"
-        />
-      </div>
-    </div>
     <template #content>
-      <ul id="cart">
-        <RouterLink to="/dashboard" class="cart_link">
-          {{ $t("dashboard") }}
-        </RouterLink>
-        <RouterLink to="/premium" class="cart_link">{{
-          $t("premium")
-        }}</RouterLink>
-        <RouterLink to="/user/account/settings" class="cart_link">
-          {{ $t("user_settings") }}
-        </RouterLink>
-        <tippy
-          class="cart_link"
-          v-tippy="{
-            interactive: true,
-            placement: 'left',
-            arrow: false,
-            theme: 'right-10',
-          }"
-        >
-          {{ $t("language") }}/Language
-          <template #content>
-            <ul id="cart_languages">
-              <li class="en" @click="setI18nLanguage(i18n, 'en')">
-                <span aria-label="🇺🇸, us, flag-us" class="emoji-mart-emoji">
-                  <span
-                    style="
-                      width: 20px;
-                      height: 20px;
-                      display: inline-block;
-                      background-image: url('https://unpkg.com/emoji-datasource-twitter@5.0.1/img/twitter/sheets-256/64.png');
-                      background-size: 5700% 5700%;
-                      background-position: 7.14286% 76.7857%;
-                    "
-                  >
-                  </span>
-                </span>
-                English
-              </li>
-              <li class="ru" @click="setI18nLanguage(i18n, 'ru')">
-                <span aria-label="🇷🇺, ru, flag-ru" class="emoji-mart-emoji">
-                  <span
-                    style="
-                      width: 20px;
-                      height: 20px;
-                      display: inline-block;
-                      background-image: url('https://unpkg.com/emoji-datasource-twitter@5.0.1/img/twitter/sheets-256/64.png');
-                      background-size: 5700% 5700%;
-                      background-position: 5.35714% 100%;
-                    "
-                  >
-                  </span>
-                </span>
-                Русский
-              </li>
-            </ul>
-          </template>
-        </tippy>
-
-        <div @click="userStore.logout()" class="cart_link logout_link">
-          {{ $t("log_out") }}
-        </div>
-      </ul>
+      <ul id="cart"></ul>
     </template>
   </tippy>
 </template>
