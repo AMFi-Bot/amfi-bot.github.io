0e9c8d8c29e5bf9f599eed2255706018cfe1d862
Vetrov Vadim <vetrovvd@yandex.ru>
2022-08-12 17:23:57 +0300

    fixed display header components on guild general module

--- src/components/Discord/Guild/Modules/BaseModule.vue
+++ src/components/Discord/Guild/Modules/BaseModule.vue
@@ -9,7 +9,7 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div :class="[$style.module, moduleBaseClass]">
+  <div class="discord-guild-module" :class="[$style.module, moduleBaseClass]">
     <div :class="[$style.module_about, moduleAboutClass]">
       <h1>{{ moduleName }}</h1>
       <p>
@@ -70,153 +70,126 @@ const props = defineProps<{
 <style global lang="scss">
 @import "@/assets/scss/library";
 
-.dropdown_elem {
-  user-select: none;
+.discord-guild-module {
+  .dropdown_elem {
+    user-select: none;
 
-  position: relative;
+    position: relative;
 
-  .dropdown_title {
-    min-height: 50px;
-    padding: 10px;
-
-    display: flex;
-    align-items: center;
-
-    cursor: pointer;
-
-    background-color: $dark_3;
-    border: 2px solid black;
-    border-radius: 5px;
-
-    @extend %hover_2;
-  }
-
-  .dropdown_content {
-    display: flex;
-    flex-direction: column;
-
-    max-height: 200px;
-    border-radius: 5px;
-
-    background-color: $dark_3;
-    border: 2px solid black;
-
-    padding: 10px;
-
-    overflow-y: auto;
-
-    z-index: 60;
-
-    .dropdown_content_elem {
-      height: 30px;
-      width: 100%;
+    .dropdown_title {
+      min-height: 50px;
+      padding: 10px;
 
       display: flex;
-      flex-direction: row;
       align-items: center;
 
       cursor: pointer;
 
-      padding: 5px 10px;
-
-      font-size: 16px;
-      color: $font_color_1;
-
-      @extend %hover_1;
+      background-color: $dark_3;
+      border: 2px solid black;
       border-radius: 5px;
 
-      .checkbox {
+      @extend %hover_2;
+    }
+
+    .dropdown_content {
+      display: flex;
+      flex-direction: column;
+
+      max-height: 200px;
+      border-radius: 5px;
+
+      background-color: $dark_3;
+      border: 2px solid black;
+
+      padding: 10px;
+
+      overflow-y: auto;
+
+      z-index: 60;
+
+      .dropdown_content_elem {
+        height: 30px;
+        width: 100%;
+
         display: flex;
+        flex-direction: row;
+        align-items: center;
 
-        width: 20px;
-        height: 20px;
+        cursor: pointer;
 
-        border: 2px solid $dark_1;
-        border-radius: 2px;
+        padding: 5px 10px;
 
-        margin-right: 5px;
-        position: relative;
-        transition: all 0.2s ease-out;
+        font-size: 16px;
+        color: $font_color_1;
 
-        &.checked {
-          background: $lightblue_button_hover;
-          border: 2px solid rgba($color: #000000, $alpha: 0);
+        @extend %hover_1;
+        border-radius: 5px;
 
-          &::after {
-            content: "";
-            position: absolute;
-            top: 2px;
-            left: 2px;
-            right: 2px;
-            bottom: 2px;
-            width: 13px;
-            height: 8px;
-            border-left: 3px solid white;
-            border-bottom: 3px solid white;
-            transform: rotate(-45deg);
-          }
+        .checkbox {
+          @include checkbox();
         }
       }
     }
   }
-}
 
-.state_checkbox {
-  position: relative;
+  .state_checkbox {
+    position: relative;
 
-  width: 100px;
-  height: 50px;
-  margin: 0;
+    width: 100px;
+    height: 50px;
+    margin: 0;
 
-  vertical-align: top;
+    vertical-align: top;
 
-  background: $dark_1;
-  border-radius: 30px;
-  outline: none;
-  cursor: pointer;
+    background: $dark_1;
+    border-radius: 30px;
+    outline: none;
+    cursor: pointer;
 
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  appearance: none;
-
-  transition: all 0.2s ease-out;
-
-  &::after {
-    content: "";
-
-    position: absolute;
-    left: 5px;
-    top: 5px;
-
-    width: 40px;
-    height: 40px;
-    background-color: #ff3535;
-    border-radius: $circle;
-
-    transform: translateX(0);
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
 
     transition: all 0.2s ease-out;
-  }
 
-  &.enabled::after,
-  &:checked::after {
-    // in this case percent element = this element
-    // In our case elements width = 40px
-    // And the parents width = 100px
-    // So sides-margin is 5px
-    // element have left-side margin = 5px
-    // 5px+40px(100%) = 45px - leftside margin for translateX(100%)
-    // If include this element = 45px+40px = 85px
-    // and 100-5px(rightside margin) = 95px
-    // So 95px - 85px = 10px
-    // So transform: translateX(calc(100% + 10px))
-    transform: translateX(calc(100% + 10px));
-    background-color: #fff;
-  }
+    &::after {
+      content: "";
 
-  &.enabled,
-  &:checked {
-    background-color: $green_button_normal;
+      position: absolute;
+      left: 5px;
+      top: 5px;
+
+      width: 40px;
+      height: 40px;
+      background-color: #ff3535;
+      border-radius: $circle;
+
+      transform: translateX(0);
+
+      transition: all 0.2s ease-out;
+    }
+
+    &.enabled::after,
+    &:checked::after {
+      // in this case percent element = this element
+      // In our case elements width = 40px
+      // And the parents width = 100px
+      // So sides-margin is 5px
+      // element have left-side margin = 5px
+      // 5px+40px(100%) = 45px - leftside margin for translateX(100%)
+      // If include this element = 45px+40px = 85px
+      // and 100-5px(rightside margin) = 95px
+      // So 95px - 85px = 10px
+      // So transform: translateX(calc(100% + 10px))
+      transform: translateX(calc(100% + 10px));
+      background-color: #fff;
+    }
+
+    &.enabled,
+    &:checked {
+      background-color: $green_button_normal;
+    }
   }
 }
 </style>
