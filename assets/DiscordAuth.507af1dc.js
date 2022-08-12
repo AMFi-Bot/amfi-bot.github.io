ada32874fb42132b144292c345d7d53f9630d7a9
Waujito <VetrovVD@yandex.ru>
2022-08-12 12:25:46 +0000

    Deploying to gh-pages from @ AMFi-Bot/amfi-bot.github.io@b70cf558833b2af4c4a034447bcf505af04f9a85 🚀

---
+++ assets/DiscordAuth.507af1dc.js
@@ -0,0 +1,1 @@
+import{d as o,u as a,w as c,c as r,o as n,_}from"./index.9756dbd0.js";const l=o({__name:"DiscordAuth",async setup(i){let e,s;const t=a();return[e,s]=c(()=>t.discord_callback(window.location.search)),await e,s(),(p,u)=>(n(),r(_))}});export{l as default};
---
+++ assets/TelegramAuth.4728155d.js
@@ -0,0 +1,1 @@
+import{d as e,u as o,c as r,o as s,_ as t}from"./index.9756dbd0.js";const m=e({__name:"TelegramAuth",setup(a){return o().login_telegram(window.location.search),(c,_)=>(s(),r(t))}});export{m as default};
