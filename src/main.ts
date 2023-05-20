9bc9ff618951760e1d46be39ef8f8a5b8d09366c
Vadim Vetrov <vetrovvd@gmail.com>
2023-05-20 19:17:19 +0300

    Added the global error handler

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,7 @@ import VueClickAway from "vue3-click-away";
 
 import DefaultLayoutVue from "./layouts/DefaultLayout.vue";
 import LoadingLayoutVue from "./layouts/LoadingLayout.vue";
+import { useErrorsStore } from "./stores/errors";
 
 const i18n = setupI18n();
 
@@ -44,5 +45,22 @@ app.component(
   "DiscordGuildLayout",
   () => import("./layouts/DiscordGuildLayout.vue")
 );
+
+app.config.errorHandler = (err, instance, info) => {
+  console.error("An unhandled error occured");
+  console.error(err);
+
+  try {
+    const errorsStore = useErrorsStore();
+
+    if (err instanceof Error)
+      errorsStore.addError(
+        `An unexpected error handled: ${err.message} ${err.stack}`
+      );
+    else errorsStore.addError(`An unexpected error handled.`);
+  } catch {
+    /* empty */
+  }
+};
 
 app.mount("#app");
