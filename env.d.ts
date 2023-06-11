e9ed45a6e09c5a03fff26e6f9891b7cf42421543
Vadim Vetrov <vetrovvd@gmail.com>
2023-06-11 16:04:23 +0300

    Added env pointer to the amfi bot's discord api url

--- env.d.ts
+++ env.d.ts
@@ -1,8 +1,20 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+  /**
+   * Points to the authorization server
+   */
   readonly VITE_AUTHORIZATION_SERVER_URL: string;
+
+  /**
+   * Client ID for the authorization server
+   */
   readonly VITE_CLIENT_ID: string;
+
+  /**
+   * Points to the amfi bot API that operates with the discord bot
+   */
+  readonly VITE_DISCORD_API_URL: string;
 }
 
 interface ImportMeta {
